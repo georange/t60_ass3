b57e40aa4e198d4bedb52095154eba2cf1caa39d
georange <georgeblub@gmail.com>
2018-11-21 10:40:29 -0800

    Add files via upload

--- diskinfo.c
+++ diskinfo.c
@@ -40,10 +40,10 @@ void name_and_label(char* memblock, char* name, char* label) {
 int get_total_size(char* memblock) {
 	int bytes_per_sector = memblock[11] + (memblock[12] << 8);
 	int total_sectors = memblock[19] + (memblock[20] << 8);
-	int total size = bytes_per_sector * total_sectors;
-	//printf("%d\n"total_size);
-	//total size = total_sectors * SECTOR_SIZE;
-	//printf("%d\n"total_size);
+	int total_size = bytes_per_sector * total_sectors;
+	printf("%d\n"total_size);
+	total size = total_sectors * SECTOR_SIZE;
+	printf("%d\n"total_size);
 
 	return total_size;
 }
